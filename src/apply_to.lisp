6603d3bfaf41b9f08f0f9b71b7d8d9af30278216
luisblazquezm <luisblazquezm@usal.es>
2019-10-09 20:21:10 +0200

    [WIP] First version of apply_to function working

--- src/apply_to.lisp
+++ src/apply_to.lisp
@@ -35,44 +35,74 @@
 
 (defun apply_to (S1 P1)
 	; Initialize local variables substitute_item, item_to_substitute and result_list to nil
-	(prog (itemS1, itemP1, substitute_item, item_to_substitute, result_list) 
+	(prog (itemS1 itemP1 substitute_item item_to_substitute var_item result_list) 
 		(cond 
 			; Finish recursivity
-			((null S1) NIL)		  
-			((null P1) NIL)	
+			((or (null S1) (null P1)) NIL)		  
 		)
 
-		(dolist (i S1 result_list)
-			(setf (nth i S1) itemS1)					; Get element i of S1 and store it in itemS1
+		(princ "S1: ")
+		(write S1)
+		(terpri)
+		(princ "P1: ")
+		(write P1)
+		(terpri)
+		(terpri)
 
-			(dolist (j P1 result_list)
+		(dolist (itemS1 S1)
+
+			(princ "ItemS1: ")
+			(write itemS1)
+			(terpri)
+
+			;(setf (nth i S1) itemS1)					; Get element i of S1 and store it in itemS1
+			(setf substitute_item (first itemS1))		; Get first element of itemS1 which is the substitute_item
+
+			(princ "substitute_item: ")
+			(write substitute_item)
+			(terpri)
+
+			(setf item_to_substitute (nth 1 itemS1))	    ; Get last element of itemS1 which is the item_to_substitute
+
+			(princ "item_to_substitute: ")
+			(write item_to_substitute)
+			(terpri)
+			
+
+			(dolist (itemP1 P1)
+
+				(princ "ItemP1: ")
+				(write itemP1)
+				(terpri)
+
 				; Get element j of P1 and store it in itemP1
-				(setf (nth j P1) itemP1)		
+				; (setf (nth j P1) itemP1)	
 
-
-				(cond ((is_variable itemS1)
-						(setf (first itemS1) substitute_item)		; Get first element of itemS1 which is the substitute_item
-						(setf (last itemS1) item_to_substitute)	    ; Get last element of itemS1 which is the item_to_substitute
-					  ) 
-					  ((is_variable itemP1)
-					  	(setf (first itemP1) substitute_item)		; Get first element of itemS1 which is the substitute_item
-						(setf (last itemP1) item_to_substitute)	    ; Get last element of itemS1 which is the item_to_substitute
-					  )
-				)		
-
+				(if (and (listp itemP1) (not(is_variable itemP1)) )                          ; if-part
+					(setf (nth (position itemP1) P1) (apply_to substitute_item P1))          ; then-part
+       			)   
+	
 				; If the element got in S1 and the element got in P1 are the same
-				((eq (extract_variable itemP1) item_to_substitute)
-					; Create a new list with the element substituted and the rest of the list we pass it to the recursivity
-					(cons substitute_item (apply_to S1 P1)) ; => (substitute_item ...)
+				; Create a new list with the element substituted and the rest of the list we pass it to the recursivity
+				; (cons substitute_item (apply_to substitute_item P1)) ; => (substitute_item ...)
+
+
+				(princ "ItemP1 position in P1: ")
+				(write (position itemP1 P1))
+				(terpri)
+
+				(if (equal itemP1 item_to_substitute)
+					(setf (nth (position itemP1 P1) P1) substitute_item)          
 				)
+
+				(terpri)
+				(terpri)
 			)
 
+			(terpri)
+			(terpri)
 		)
 
-		; If the previous conditions are not accomplished, returns the list substituting the members
-		; Find the index of the term to substitute in the list and replace it with the substitution term
-		(setf (nth (get_item_index_in_list var_to_substitute list_of_predicates)  
-		            list_of_predicates) substitution_term) 
+		(return P1) 
 	)
-		
 )
